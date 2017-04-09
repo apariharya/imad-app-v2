d7cb86c6af6c027cfab09814c7d0d5782aeca9c0
Shubham Agnihotri <shubham.a.82@gmail.com>
2017-04-09 16:00:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,12 +1,11 @@
 console.log('Loaded!');
 alert('Hi everyone');
 var img = document.getElementById('madi');
-var marginLeft=0;
+var marginLeft = 0;
 function moveRight(){
     marginLeft = marginLerft + 5;
-    img.style.marginLeft=marginLeft + 'px';
-
+    img.style.marginLeft = marginLeft + 'px';
 }
-img.onclick = function(){
+img.onclick = function() {
     var interval = setInterval(moveRight,50);
 };
