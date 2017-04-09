68aedea45ad3476a2fbde148ef2cbe0bbe45c8e6
Shubham Agnihotri <shubham.a.82@gmail.com>
2017-04-09 14:31:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -114,8 +114,8 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/index.html', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
 app.get('/ui/madi.png', function (req, res) {
